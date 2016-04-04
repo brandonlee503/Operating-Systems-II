df2c60cc37e1058400e7eb60042fb6f5728dbc5c
Brandon Lee <brandonlee503@gmail.com>
2016-04-04 15:56:39 -0700

    Add skeleton layout for concurrency assignment 1

---
+++ concurrency/concurrency1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+struct bufferItem {
+    int number;
+    int sleepTime;
+};
+
+struct bufferArray {
+    struct bufferItem buffer[32];
+};
+
+void produce() {
+
+}
+
+void consume() {
+
+}
+
+int randomNumberGenerator() {
+
+}
+
+int main(int argc, char **argv) {
+    
+}
