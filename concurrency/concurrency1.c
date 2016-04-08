6dfc3514d54aec67a649104be41f24df73757773
Brandon Lee <brandonlee503@gmail.com>
2016-04-08 00:12:42 -0700

    Implement Mersenne Twister and ASM instruction rdrand (only partially working...)

--- concurrency/concurrency1.c
+++ concurrency/concurrency1.c
@@ -9,7 +9,6 @@
 int eax, ebx, ecx, edx;
 int producerIndex, consumerIndex;
 
-
 struct bufferItem {
     int number;
     int sleepTime;
@@ -27,10 +26,42 @@ void consume() {
 
 }
 
-int randomNumberGenerator() {
-    
+void setRegisters() {
+    // http://www.codeproject.com/Articles/15971/Using-Inline-Assembly-in-C-C
+    __asm__ __volatile__("cpuid;" : "=a"(eax), "=b"(ebx), "=c"(ecx), "=d"(edx) : "a"(eax));
 }
 
-int main(int argc, char **argv) {
-    
+// https://hackage.haskell.org/package/crypto-random-0.0.9/src/cbits/rdrand.c
+int rdrand(int *number) {
+    unsigned char err;
+    __asm__ __volatile__("rdrand %0; setc %1" : "=r" (*number), "=qm" (err));
+    return (int) err;
+}
+
+int randomNumberGenerator(int min, int max) {
+    int number = 0;
+
+    setRegisters();
+
+    // Check if system supports rdrand by checking bit 30 of ECX register.
+    // http://www.intel.com/content/www/us/en/architecture-and-technology/64-ia-32-architectures-software-developer-manual-325462.html
+    // http://stackoverflow.com/questions/523724/c-c-check-if-one-bit-is-set-in-i-e-int-variable
+    if (ecx & 1<<30) {
+        rdrand(&number);
+    } else {
+        number = (int)genrand_int32();
+    }
+
+    number %= (max - min);
+    if (number < min) {
+        number = min;
+    }
+    printf("MT number: %d\n", number);
+}
+
+int main(int argc, char *argv) {
+   printf("In - main()\n");
+   while(1) {
+       randomNumberGenerator(1, 100);
+   }
 }
